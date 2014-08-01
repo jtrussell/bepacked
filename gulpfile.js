afd7cf75c473ce1a5da3bcca0cf2de6cdc404656
Justin <jus.russell@gmail.com>
2014-08-01 17:19:15 -0400

    chore: Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+
+var gulp = require('gulp')
+  , jshint = require('gulp-jshint')
+  , mocha = require('gulp-mocha');
+
+gulp.task('lint-src', function() {
+  return gulp.src('lib/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('lint-test', function() {
+  return gulp.src('test/*-test.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function() {
+  return gulp.src('test/*-test.js', {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('lib/*.js', ['lint-src', 'test']);
+  gulp.watch('test/*-test.js', ['lint-test', 'test']);
+});
+
+gulp.task('default', [
+  'lint-src',
+  'lint-test',
+  'test'
+]);
---
+++ test/packerator-test.js
@@ -0,0 +1,49 @@
+/*global describe, it, beforeEach */
+/*jshint expr:true */
+
+var expect = require('chai').expect
+  , packerator = require('../');
+
+describe('packerator', function() {
+
+  it('should not generate an error', function() {
+    expect(false).to.be.ok;
+  });
+  
+  it('should inline local JavaScript sources', function() {
+    expect(false).to.be.ok;
+  });
+
+  it('should inline external JavaScript sources', function() {
+    expect(false).to.be.ok;
+  });
+
+  it('should inline local stylesheets', function() {
+    expect(false).to.be.ok;
+  });
+
+  it('should inline external stylesheets', function() {
+    expect(false).to.be.ok;
+  });
+
+  it('should datauri-ify local images', function() {
+    expect(false).to.be.ok;
+  });
+
+  it('should datauri-ify external images', function() {
+    expect(false).to.be.ok;
+  });
+
+  it('should datauri-ify images in stylesheets', function() {
+    expect(false).to.be.ok;
+  });
+
+  it('should datauri-ify images in inline styles', function() {
+    expect(false).to.be.ok;
+  });
+
+  it('should have a streaming interface', function() {
+    expect(false).to.be.ok;
+  });
+
+});
